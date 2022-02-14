c5eb9973af3fcc1df1ee1f515b8acd19b09eb006
EliasRonefors <elias.ronefors@elev.ga.lbs.se>
2022-02-14 11:44:53 +0100

    Begun debugging shooting, created Bullet PreFab

--- Gruppprojekt Profilvecka/Assets/Scripts/ShooterScript.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/ShooterScript.cs
@@ -15,9 +15,24 @@ public class ShooterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        AimShooter();
+        PlayerInput();
+    }
+
+    public void AimShooter()
+    {
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 dir = Input.mousePosition - pos;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    public void PlayerInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left button");
+
+        }
     }
 }
