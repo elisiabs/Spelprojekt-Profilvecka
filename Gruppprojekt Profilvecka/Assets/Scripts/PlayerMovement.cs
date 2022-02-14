d68d0a7cc35aadcf9326a5ad9e6b9d1cc4c907b7
elisia <elisia.boikovsivertoft@elev.ga.lbs.se>
2022-02-14 11:09:03 +0100

    player can now only jump if timesjumped < 1

--- Gruppprojekt Profilvecka/Assets/Scripts/PlayerMovement.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    
     public float jumpHeight;
+    public int timesJumped;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if(timesJumped < 1)
         {
-            GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-            rb.AddForce(Vector3.up * jumpHeight);
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+                rb.AddForce(Vector3.up * jumpHeight);
+                timesJumped++;
+            }
+        }
+        
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            timesJumped = 0;
         }
     }
 }
