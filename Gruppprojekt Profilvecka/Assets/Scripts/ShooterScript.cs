ece680acd047c206dd9d1c906802be406265d9a3
EliasRonefors <elias.ronefors@elev.ga.lbs.se>
2022-02-14 11:30:06 +0100

    Updated ShooterScript.cs to aim gun

--- Gruppprojekt Profilvecka/Assets/Scripts/ShooterScript.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/ShooterScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ShooterScript : MonoBehaviour
 {
+    public Vector3 worldPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class ShooterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
