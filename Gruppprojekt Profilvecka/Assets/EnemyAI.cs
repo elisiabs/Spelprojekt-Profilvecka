3fc4a79d4b092f75b812b110bd0138285a5dd630
HassanAfroGod <almojtaba.karimhassan@elev.ga.lbs.se>
2022-02-17 10:21:24 +0100

    added enemy AI

--- Gruppprojekt Profilvecka/Assets/EnemyAI.cs
+++ Gruppprojekt Profilvecka/Assets/EnemyAI.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+<<<<<<< Updated upstream
 using UnityEngine;
 using Pathfinding;
 
@@ -121,3 +122,22 @@ public class EnemyAI : MonoBehaviour {
     }
 }   
 
+=======
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+>>>>>>> Stashed changes
