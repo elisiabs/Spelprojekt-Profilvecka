50d46ced0795cd1f90877cfacd3c18670aec5a9a
Elias Ronefors <elias.ronefors@elev.ga.lbs.se>
2022-02-18 16:50:00 +0100

    Added Player Health indicators

--- Gruppprojekt Profilvecka/Assets/Scripts/PlayerHealth.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health;
+    public float redFlashSeconds;
     public float invincibilityTime;
     public bool invincible = false;
-    public SpriteRenderer sprite;
+    public SpriteRenderer playerSprite;
+    public Image[] hearts;
+    public Sprite brokenHeart;
 
     void Start()
     {
@@ -15,19 +19,40 @@ public class PlayerHealth : MonoBehaviour
     }
     void Update()
     {
-        if(health <= 0)
-        {
-            //die
-            Debug.Log("You died.");
-        }
+
     }
     public void damagePlayer(float damage)
     {
         if (!invincible)
         {
             health -= damage;
-            sprite.color = Color.red;
             StartCoroutine(InvincibilityTime(invincibilityTime));
+            StartCoroutine(RedFlash(redFlashSeconds));
+        }
+
+        switch (health)
+        {
+            case 0:
+                {
+                    //TODO: implement die
+                    Debug.Log("You died.");
+
+                    //hearts[0].color = new Color(0, 0, 0, 0);
+                    hearts[0].sprite = brokenHeart;
+                    break;
+                }
+            case 1:
+                {
+                    //hearts[1].color = new Color(0, 0, 0, 0);
+                    hearts[1].sprite = brokenHeart;
+                    break;
+                }
+            case 2:
+                {
+                    //hearts[2].color = new Color(0,0,0,0);
+                    hearts[2].sprite = brokenHeart;
+                    break;
+                }
         }
     }
 
@@ -36,6 +61,12 @@ public class PlayerHealth : MonoBehaviour
         invincible = true;
         yield return new WaitForSeconds(seconds);
         invincible = false;
-        sprite.color = Color.white;
+    }
+
+    IEnumerator RedFlash(float seconds)
+    {
+        playerSprite.color = Color.red;
+        yield return new WaitForSeconds(seconds);
+        playerSprite.color = Color.white;
     }
 }
