7192c6dd7b4658b3e4c596e63aae5e5f2719b9d1
EliasRonefors <elias.ronefors@elev.ga.lbs.se>
2022-03-08 16:01:36 +0100

    lol

--- Gruppprojekt Profilvecka/Assets/Scripts/Destruktable.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/Destruktable.cs
@@ -12,19 +12,6 @@ public class Destruktable : MonoBehaviour
         DestruktableTileMap = GetComponent<Tilemap>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("PlayerProjectile"))
-        {
-            Vector3 hitPosision = Vector3.zero;
-            foreach (ContactPoint2D hit in collision.contacts)
-            {
-                hitPosision.x = hit.point.x - 0.01f * hit.normal.x;
-                hitPosision.y = hit.point.y - 0.01f * hit.normal.y;
-                DestruktableTileMap.SetTile(DestruktableTileMap.WorldToCell(hitPosision), null);
-            }
-        }
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("PlayerProjectile"))
