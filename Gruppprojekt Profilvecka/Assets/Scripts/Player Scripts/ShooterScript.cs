a5a9d5b9ec149ace383abf688b07f58b5c73e101
Elias Ronefors <elias.ronefors@elev.ga.lbs.se>
2022-03-18 21:33:20 +0100

    Begun implementing code for secondary weapon

--- Gruppprojekt Profilvecka/Assets/Scripts/Player Scripts/ShooterScript.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/Player Scripts/ShooterScript.cs
@@ -5,21 +5,34 @@ using UnityEngine.UI;
 
 public class ShooterScript : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject pivotPoint;
-    public GameObject bullet;
-    public GameObject bulletSpawn;
-    public Slider slider;
-    public GameObject backLightSprite;
-    public Animator animator;
-    public GameObject light;
-    public float recoilAmount;
-    Rigidbody2D playerRb;
+    [Header("GameObjects")]
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject pivotPoint;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject bulletSpawn;
+    [SerializeField] private BoxCollider2D pushCollider;
+    [SerializeField] private GameObject backLightSprite;
+    [SerializeField] private GameObject light;
 
+    [Header("GameObjects used only for shooter change")]
+    [SerializeField] private GameObject Shooter1;
+    [SerializeField] private GameObject Shooter2;
+
+    [Header("Components")]
+    [SerializeField] private Slider slider;
+    [SerializeField] private Animator animator;
+    private Rigidbody2D playerRb;
+
+    [Header("Variables")]
     private float angle;
-    public float bulletVelocity;
-    public float cooldownSpeed;
-    float cooldown;
+    private float cooldown;
+    [Header("Shooter 1 Variables")]
+    [SerializeField] private float recoilAmount;
+    [SerializeField] private float bulletVelocity;
+    [SerializeField] private float cooldownSpeed;
+    [Header("Shooter 2 Variables")]
+    [SerializeField] private float recoilAmount2;
+
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +44,7 @@ public class ShooterScript : MonoBehaviour
     void Update()
     {
         AimShooter();
+        SwitchWeapon();
         PlayerInput();
         
         if(cooldown < 1)
@@ -68,14 +82,36 @@ public class ShooterScript : MonoBehaviour
         }
     }
 
+    public void SwitchWeapon()
+    {
+        if (Input.GetMouseButtonDown(0) && cooldown >= 1) //Left click
+        {
+            Shooter1.SetActive(true);
+            Shooter2.SetActive(false);
+        }
+        else if (Input.GetMouseButtonDown(1) && cooldown >= 1) //Right click //TODO: implement game manager to manage if player has unlocked this yet.
+        {
+            Shooter1.SetActive(false);
+            Shooter2.SetActive(true);
+        }
+    }
+
     public void PlayerInput()
     {
-        if (Input.GetMouseButtonDown(0) && cooldown >= 1)
+        if (Input.GetMouseButtonDown(0) && cooldown >= 1 && Shooter1.activeInHierarchy)
         {
             GameObject obj = Instantiate(bullet, bulletSpawn.transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
             obj.GetComponent<Rigidbody2D>().velocity = transform.right * bulletVelocity;
             cooldown = 0;
             playerRb.velocity = -transform.right * recoilAmount;
+            StartCoroutine(muzzleFlash());
+            animator.SetTrigger("Shoot");
+        }
+        else if(Input.GetMouseButtonDown(1) && cooldown <= 1 && Shooter2.activeInHierarchy)
+        {
+            cooldown = 0;
+            playerRb.velocity = Vector2.zero;
+            playerRb.velocity = -transform.right * recoilAmount2;
             StartCoroutine(muzzleFlash());
             animator.SetTrigger("Shoot");
         }
@@ -87,4 +123,21 @@ public class ShooterScript : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
         light.SetActive(false);
     }
+
+    /*private void OnTriggerStay2D(Collider2D col)
+    {
+        Debug.Log("lol");
+        
+        Debug.Log("OnTriggerStay2D");
+        if (Input.GetMouseButtonDown(1) && cooldown <= 1 && Shooter2.activeInHierarchy)
+        {
+            Debug.Log("Player Right click while col");
+            if (col.gameObject.layer == 9) //9 is layer "Ghost".
+            {
+                Debug.Log("Enemy pushed");
+                col.gameObject.GetComponent<Rigidbody2D>().velocity = -transform.right * 6000;
+            }
+        }
+    }
+    */
 }
