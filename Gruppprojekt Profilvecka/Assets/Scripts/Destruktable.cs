bee10e60c8f68c0a5dc7bac77dfde8c396889443
PanagiotisDaBest <90244906+PanagiotisDaBest@users.noreply.github.com>
2022-04-05 14:48:39 +0200

    Door has issues

--- Gruppprojekt Profilvecka/Assets/Scripts/Destruktable.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/Destruktable.cs
@@ -11,12 +11,26 @@ public class Destruktable : MonoBehaviour
     {
         if (collision.gameObject.tag == ("PlayerProjectile"))
         {
-                for (int i = 0; i < 4; i++)
-                {  
-                    Rigidbody2D rigidbody = DoorPieces[i].gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
-                    DoorPieces[i].parent = null;
+            ContactFilter2D contactFilter = new ContactFilter2D();
+            List<Collider2D> hitColliders = new List<Collider2D>();
+            collision.OverlapCollider(contactFilter, hitColliders);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Rigidbody2D rigidbody = DoorPieces[i].gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+                for (int i2 = 0; i2 < 4; i2++)
+                {
+                    if (hitColliders[i2].gameObject.tag == "PlayerProjectile")
+                    {
+                        Vector2 push = (DoorPieces[i].transform.position - hitColliders[i2].transform.position);
+                        DoorPieces[i].GetComponent<Rigidbody2D>().velocity = push;
+                    }
                 }
-            
+                
+                DoorPieces[i].parent = null;
+
+                Destroy(DoorPieces[i].gameObject, 2f);
+            }
             Destroy(gameObject);
         }
     }
