1ea551966b3b4cee6699648544d14569e32ddcc0
elisia <elisia.boikovsivertoft@elev.ga.lbs.se>
2022-04-19 11:33:44 +0200

    the pause menu is done !

--- Gruppprojekt Profilvecka/Assets/Scripts/UI & Menu Scripts/PauseMenuScript.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/UI & Menu Scripts/PauseMenuScript.cs
@@ -12,23 +12,46 @@ public class PauseMenuScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isPaused = Time.timeScale == 0;
         if (Input.GetKeyDown(KeyCode.Escape))
-        {   
-            EscPauseButton.SetActive(!EscPauseButton.activeInHierarchy);
-            EscPauseMenu.SetActive(!EscPauseMenu.activeInHierarchy);
+        {
+            if (isPaused == false)
+            {
+                Time.timeScale = 0;
+                EscPauseButton.SetActive(false);
+                EscPauseMenu.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                EscPauseButton.SetActive(true);
+                EscPauseMenu.SetActive(false);
+            }
         }
+        /*if(inPause = true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 1;
+        }*/
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        EscPauseButton.SetActive(false);
+        EscPauseMenu.SetActive(true);
     }
 
     public void Continue()
     {
         EscPauseButton.SetActive(true);
         EscPauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 
     public void Menu()
