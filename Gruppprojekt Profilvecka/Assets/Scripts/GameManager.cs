c87c7eefe25c7c82669c47ddbd10b1746449148a
EliasRonefors <elias.ronefors@elev.ga.lbs.se>
2022-05-03 13:50:23 +0200

    Update GameManager.cs

--- Gruppprojekt Profilvecka/Assets/Scripts/GameManager.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/GameManager.cs
@@ -9,6 +9,21 @@ public class GameManager : MonoBehaviour
 
     private Vector2 spawnPosition;
 
+    public static GameManager Instance;
+
+    void Awake() // Slightly stolen :)
+    {
+        if (Instance == null) // If there is no instance already
+        {
+            DontDestroyOnLoad(gameObject); // Keep the GameObject, this component is attached to, across different scenes
+            Instance = this;
+        }
+        else if (Instance != this) // If there is already an instance and it's not `this` instance
+        {
+            Destroy(gameObject); // Destroy the GameObject, this component is attached to
+        }
+    }
+
     void Start()
     {
         spawnPosition = player.transform.position;
