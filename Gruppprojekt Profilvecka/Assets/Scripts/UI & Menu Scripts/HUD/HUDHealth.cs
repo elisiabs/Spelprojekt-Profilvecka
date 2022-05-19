a7cf2190fd954eb46df10f1b0469fe4fef613044
Elias Ronefors <elias.ronefors@elev.ga.lbs.se>
2022-05-19 20:18:37 +0200

    semi-fixed hearts positions and semi made them work

--- Gruppprojekt Profilvecka/Assets/Scripts/UI & Menu Scripts/HUD/HUDHealth.cs
+++ Gruppprojekt Profilvecka/Assets/Scripts/UI & Menu Scripts/HUD/HUDHealth.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class HUDHealth : MonoBehaviour
 {
@@ -9,13 +10,16 @@ public class HUDHealth : MonoBehaviour
 
     private float currentHealth;
     public int maxHealth;
-    [SerializeField] private Vector2 rPosition;
+    private float lastFrameHealth;
+    private float lastHealthDifference;
+    private Vector2 rPosition;
     private GameObject parent;
-    
+    [SerializeField] private float healthY;
+    [SerializeField] private float healthXSpace;
 
     private Image[] hearts;
-    [SerializeField] private Sprite heart;
-    [SerializeField] private Sprite brokenHeart;
+    [SerializeField] private Sprite heartSprite;
+    [SerializeField] private Sprite brokenHeartSprite;
 
     // Start is called before the first frame update
     void Start()
@@ -24,19 +28,31 @@ public class HUDHealth : MonoBehaviour
         currentHealth = gameManager.player.playerHealth.health;
         parent = gameObject;
 
-        initializeHearts(maxHealth);
+        rPosition = new Vector2(Screen.width/2, healthY);
+        hearts = new Image[maxHealth];
+
+        InitializeHearts();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-    }
-
-    private void initializeHearts(int amount)
-    {
-        for (int i = 0; i < amount; i++)
+        currentHealth = gameManager.player.playerHealth.health;
+        lastHealthDifference = lastFrameHealth - currentHealth;
+        if (currentHealth < lastFrameHealth)
         {
+            DamageHearts((int)lastFrameHealth - (int)currentHealth);
+            Debug.Log("Health lowered");
+        }
+        lastFrameHealth = gameManager.player.playerHealth.health;
+    }
+
+    private void InitializeHearts()
+    {
+        for (int i = 0; i < maxHealth; i++)
+        {
+            float desiredY = (rPosition.x + (healthXSpace * (i + 1)));
+            Vector2 desiredPosition = new Vector2(desiredY, rPosition.y);
+
             GameObject objToSpawn = new GameObject("Empty");
             GameObject obj = Instantiate(objToSpawn);
             obj.transform.SetParent(parent.transform);
@@ -44,20 +60,24 @@ public class HUDHealth : MonoBehaviour
             
             RectTransform rTransform = obj.GetComponent<RectTransform>();
             rTransform.localScale = new Vector3(1, 1, 1);
-            rTransform.position = rPosition;
+            rTransform.position = desiredPosition;
             rTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 43.75f);
             rTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 43.75f);
             
             Image image = obj.AddComponent<Image>();
-            image.sprite = heart;
+            image.sprite = heartSprite;
+            hearts[i] = image;
 
-            shit(rTransform);
         }
     }
 
-    IEnumerator shit(RectTransform rTransform)
+    public void DamageHearts(int damage)
     {
-        yield return new WaitForSeconds(0.1f);
-        rTransform.position = rPosition;
+        for (int i = hearts.Length - 1; i >= currentHealth - damage; i--)
+        {
+            Debug.Log("Damaged hearts. i = " + i);
+
+            hearts[i].sprite = brokenHeartSprite;
+        }
     }
 }
